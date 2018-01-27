cab7ad1d57c387744afedb348642d8d69b86fc67
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-01-27 18:35:08 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,7 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def search
-    
+  def self.search(query)
+    where("name LIKE ?", "%#{query}%")
   end
 end
